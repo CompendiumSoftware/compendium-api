8190206dc9e1c68bf471ed4f84ba58c025de3db4
Dan McFadden <d3mcfadden@gmail.com>
2014-07-23 15:11:00 -0400

    allow network and schedule at to be passed properly

--- lib/nretnil/compendium-api/callback.rb
+++ lib/nretnil/compendium-api/callback.rb
@@ -15,8 +15,15 @@ module Nretnil
         @session = s
       end
 
-      def fire(name, arguments)
+      def fire(name, arguments, network_id = nil, schedule_at = nil)
           query = { :EventName => name, :EventArgs => arguments.to_json }
+          if network_id
+            query[:NetworkId] = network_id
+          end
+          if schedule_at
+            query[:ScheduleAt] = schedule_at
+          end
+
           response = @session.post( '/app/callback/fire', query )
           parsed = response.parsed_response
           if parsed.key?("Error")
